32fb2a0dff13df97af19bb26377764ab6656305c
josev <josev@PC13>
2019-02-21 14:18:32 +0100

    Merge origin/master

--- controladores/controladorAdministrador.php
+++ controladores/controladorAdministrador.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 require_once '../clasesdb/administradorDB.php';
 require_once '../clasesdb/eventoDB.php';
@@ -8,58 +9,51 @@ if (isset($_REQUEST["accion"])) {
     $accion = $_REQUEST["accion"];
     $accion = strtoupper($accion);
     $accion = str_replace(" ", "", $accion);
-    
-    switch($accion){
+
+    switch ($accion) {
         case "ACCEDER":
-            $resultado=administradorDB::acceder($_REQUEST["usuario"], $_REQUEST["clave"]);
-            if ($resultado){
-                $_SESSION["administrador"]=new Administrador($_REQUEST["usuario"],$_REQUEST["clave"]);
-                $url="Location:../menuAdministrador.php";
-            }else{
-                $url="Location:../administrador.php?mensaje=Error%20Loggin";
+            $resultado = administradorDB::acceder($_REQUEST["usuario"], $_REQUEST["clave"]);
+            if ($resultado) {
+                $_SESSION["administrador"] = new Administrador($_REQUEST["usuario"], $_REQUEST["clave"]);
+                $url = "Location:../menuAdministrador.php";
+            } else {
+                $url = "Location:../administrador.php?mensaje=Error%20Loggin";
             }
-        break;
-        
+            break;
+
         case "CREAREVENTO":
             $ok = eventoDB::crearEvento($_REQUEST);
-            if($ok == 0){
+            if ($ok == 0) {
                 echo "Evento creado correctamente";
             } else {
                 echo $ok;
             }
             $url = "Location:../menuAdministrador.php";
-        break;
-    
-    
+            break;
     }
     header($url);
 }
 
-function subirFicheros($nombreproyecto){
+function subirFicheros($nombreproyecto) {
     //Como el elemento es un arreglos utilizamos foreach para extraer todos los valores
-	foreach($_FILES["archivo"]['tmp_name'] as $key => $tmp_name)
-	{
-		//Validamos que el archivo exista
-		if($_FILES["archivo"]["name"][$key]) {
-			$filename = $_FILES["archivo"]["name"][$key]; //Obtenemos el nombre original del archivo
-			$source = $_FILES["archivo"]["tmp_name"][$key]; //Obtenemos un nombre temporal del archivo
-			
-			$directorio = '../proyectos'.$nombreproyecto.'/'; //Declaramos un  variable con la ruta donde guardaremos los archivos
-			
-			//Validamos si la ruta de destino existe, en caso de no existir la creamos
-			if(!file_exists($directorio)){
-				mkdir($directorio, 0777) or die("No se puede crear el directorio de extracci&oacute;n");	
-			}
-			
-			$dir=opendir($directorio); //Abrimos el directorio de destino
-			$target_path = $directorio.'/'.$filename; //Indicamos la ruta de destino, así como el nombre del archivo
-			
-			//Movemos y validamos que el archivo se haya cargado correctamente
-			//El primer campo es el origen y el segundo el destino
-                        move_uploaded_file($source, $target_path);
-			closedir($dir); //Cerramos el directorio de destino
-		}
-	}
-    
-    
+    foreach ($_FILES["archivo"]['tmp_name'] as $key => $tmp_name) {
+        //Validamos que el archivo exista
+        if ($_FILES["archivo"]["name"][$key]) {
+            $filename = $_FILES["archivo"]["name"][$key]; //Obtenemos el nombre original del archivo
+            $source = $_FILES["archivo"]["tmp_name"][$key]; //Obtenemos un nombre temporal del archivo
+
+            $directorio = '../proyectos' . $nombreproyecto . '/'; //Declaramos un  variable con la ruta donde guardaremos los archivos
+            //Validamos si la ruta de destino existe, en caso de no existir la creamos
+            if (!file_exists($directorio)) {
+                mkdir($directorio, 0777) or die("No se puede crear el directorio de extracci&oacute;n");
+            }
+
+            $dir = opendir($directorio); //Abrimos el directorio de destino
+            $target_path = $directorio . '/' . $filename; //Indicamos la ruta de destino, así como el nombre del archivo
+            //Movemos y validamos que el archivo se haya cargado correctamente
+            //El primer campo es el origen y el segundo el destino
+            move_uploaded_file($source, $target_path);
+            closedir($dir); //Cerramos el directorio de destino
+        }
+    }
 }
